f0fa0501524873a42a998ce14d9e78c7fa4696ad
arburhan <buthan190629@outlook.com>
2022-07-01 06:12:38 +0600

    show all todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from './Components/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
-import ToDo from './Components/Pages/ToDo';
 import Login from './Components/Pages/Login';
 import SignUp from './Components/Pages/SignUp';
 import CompleteTask from './Components/Pages/CompleteTask';
 import Calendar from './Components/Pages/Calender/Calendar';
 import Home from './Components/Pages/Home';
+import Todo from './Components/Pages/ToDo/Todo';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signUp' element={<SignUp></SignUp>}></Route>
-        <Route path='/toDo' element={<ToDo></ToDo>}></Route>
+        <Route path='/toDo' element={<Todo></Todo>}></Route>
         <Route path='/completeTask' element={<CompleteTask></CompleteTask>}></Route>
         <Route path='/calendar' element={<Calendar></Calendar>}></Route>
         <Route path='/' element={<Home></Home>}></Route>
--- src/Components/Pages/ToDo/Todo.js
+++ src/Components/Pages/ToDo/Todo.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import SingleTask from '../SingleTask';
 
 const Todo = () => {
+    const { data: taskLists, isLoading, refetch } = useQuery(['allTask'], () =>
+        fetch(`ToDo.json`)
+            .then(res => res.json())
+    )
+    if (isLoading) {
+        return <div>
+            <progress class="progress w-56"></progress>
+        </div>
+    }
+
     return (
-        <div>
+        <div className='my-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3' >
+                {taskLists?.map(taskList => <SingleTask key={taskList._id} taskList={taskList} refetch={refetch}> </SingleTask>)}
+            </div>
 
         </div>
     );
